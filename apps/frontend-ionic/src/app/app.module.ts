2e1f6c0f1e6e7c9ee9fb0126deec2c66c6e6527a
Rares Golea <rgolea@gmail.com>
2020-10-25 20:20:50 +0100

    Added firebase

--- apps/frontend-ionic/src/app/app.module.ts
+++ apps/frontend-ionic/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './app.router';
 import { RouterModule } from '@angular/router';
 import { HorizontalScrollModule } from "@adventist/horizontal-scroll";
+import { AngularFireModule } from '@angular/fire';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent],
@@ -15,7 +16,17 @@ import { HorizontalScrollModule } from "@adventist/horizontal-scroll";
     IonicModule.forRoot(),
     AppRoutingModule,
     RouterModule.forRoot([]),
-    HorizontalScrollModule
+    HorizontalScrollModule,
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyAoCTUjpyaDjQZxZi3npE1-ijhmFC8o9rI",
+      authDomain: "newsletter-3cc38.firebaseapp.com",
+      databaseURL: "https://newsletter-3cc38.firebaseio.com",
+      projectId: "newsletter-3cc38",
+      storageBucket: "newsletter-3cc38.appspot.com",
+      messagingSenderId: "797249201952",
+      appId: "1:797249201952:web:1f6f88b8d368ead54f1945",
+      measurementId: "G-4G9E8D81Y9"
+    })
   ],
   providers: [],
   bootstrap: [AppComponent],
