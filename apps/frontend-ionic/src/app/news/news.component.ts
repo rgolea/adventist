9123876c1376944dc93f012d051c8474d29efed6
Rares Golea <rgolea@gmail.com>
2020-11-15 20:21:26 +0100

    Displaying news

--- apps/frontend-ionic/src/app/news/news.component.ts
+++ apps/frontend-ionic/src/app/news/news.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, OnDestroy } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { ActivatedRoute } from '@angular/router';
+import { ReplaySubject, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { News } from "@adventist/interfaces";
 
 @Component({
   selector: 'adventist-news',
@@ -6,11 +11,25 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   styleUrls: ['./news.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class NewsComponent implements OnInit {
+export class NewsComponent implements OnInit, OnDestroy {
+  private destroy$ = new Subject();
+  public news$ = new ReplaySubject<News>(1);
 
-  constructor() { }
+  constructor(
+    private readonly afs: AngularFirestore,
+    private readonly route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    const collection = this.afs.collection('news');
+    collection.doc<News>(id).valueChanges().pipe(
+      takeUntil(this.destroy$)
+    ).subscribe(val => this.news$.next(val));
+  }
+
+  ngOnDestroy(){
+    this.destroy$.next();
   }
 
 }
--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
   projects: [
     '<rootDir>/apps/frontend-ionic',
     '<rootDir>/libs/horizontal-scroll',
+    '<rootDir>/libs/interfaces',
   ],
 };
