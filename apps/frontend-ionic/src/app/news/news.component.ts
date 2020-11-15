24f626567954f3dd4a277baf21258dcf6c57cc58
Facundo Rodriguez <rodriguez.facu23@gmail.com>
2020-11-15 18:48:15 -0300

    feat: agrega navigacion hacia atras

--- apps/frontend-ionic/src/app/news/news.component.ts
+++ apps/frontend-ionic/src/app/news/news.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit, ChangeDetectionStrategy, OnDestroy } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ChangeDetectionStrategy,
+  OnDestroy,
+} from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { ActivatedRoute } from '@angular/router';
 import { ReplaySubject, Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
-import { News } from "@adventist/interfaces";
+import { News } from '@adventist/interfaces';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'adventist-news',
   templateUrl: './news.component.html',
   styleUrls: ['./news.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NewsComponent implements OnInit, OnDestroy {
   private destroy$ = new Subject();
@@ -17,19 +23,25 @@ export class NewsComponent implements OnInit, OnDestroy {
 
   constructor(
     private readonly afs: AngularFirestore,
-    private readonly route: ActivatedRoute
-  ) { }
+    private readonly route: ActivatedRoute,
+    private readonly navController: NavController
+  ) {}
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
     const collection = this.afs.collection('news');
-    collection.doc<News>(id).valueChanges().pipe(
-      takeUntil(this.destroy$)
-    ).subscribe(val => this.news$.next(val));
+    collection
+      .doc<News>(id)
+      .valueChanges()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((val) => this.news$.next(val));
   }
 
-  ngOnDestroy(){
+  goBack(): void {
+    this.navController.back();
+  }
+
+  ngOnDestroy() {
     this.destroy$.next();
   }
-
 }
